6a60daf2536e8cc4bf6f9d8348409e064ee6002d
Ajay Sai <mac@Ajays-iMAC.local>
2024-08-30 21:33:59 +0530

    add: deploy workflows

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home'
 
 
 const App = () => {
   return (
     <div className='app'>
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route index element={<Home />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   )
 }
